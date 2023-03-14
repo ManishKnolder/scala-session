ed4c385a450eb160bd7b08ca96995c9c7bb07125
Manish Mishra <manish.mishra@knoldus.com>
2023-03-15 00:58:54 +0530

    Added Factorial by Tail Recursion

---
+++ session_1/session-1-assignment-2/src/main/scala/FactorialRecursion.scala
@@ -0,0 +1,15 @@
+object FactorialRecursion extends App {
+  //factorial method is used to calculate the factorial of the passed number as parameter.
+  def factorial(number: Int, accumulator: Int): Int = {
+    try {
+      if (number.isNaN) throw new NumberFormatException()
+    }
+    catch {
+      case ex: NumberFormatException => {
+        ex.getMessage()
+      }
+    }
+    if (number <= 1) accumulator
+    else factorial(number - 1, accumulator * number)
+  }
+}
