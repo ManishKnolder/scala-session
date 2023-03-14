7c59d67504a12876434e308f7386b019fe9459a3
Manish Mishra <manish.mishra@knoldus.com>
2023-03-15 02:04:23 +0530

    Added factorial file by using tail recursion

--- session_1/session-1-assignment-2/src/main/scala/FactorialRecursion.scala
+++ session_1/session-1-assignment-2/src/main/scala/FactorialRecursion.scala
@@ -2,14 +2,15 @@ object FactorialRecursion extends App {
   //factorial method is used to calculate the factorial of the passed number as parameter.
   def factorial(number: Int, accumulator: Int): Int = {
     try {
-      if (number.isNaN) throw new NumberFormatException()
+      if (number < 0) throw new NumberFormatException
     }
     catch {
       case ex: NumberFormatException => {
-        ex.getMessage()
+        ex.getMessage
       }
     }
     if (number <= 1) accumulator
     else factorial(number - 1, accumulator * number)
   }
+
 }
