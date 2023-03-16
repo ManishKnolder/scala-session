0efb2de7c49c463e0c56fb87961a2d30c26c41d1
Manish Mishra <124979629+ManishKnolder@users.noreply.github.com>
2023-03-16 11:53:40 +0530

    Updated
    Added Test cases

--- session_1/session-1-assignment-3/src/test/scala/SortingProgramTest.scala
+++ session_1/session-1-assignment-3/src/test/scala/SortingProgramTest.scala
@@ -15,18 +15,4 @@ class SortingProgramTest extends AnyFlatSpec {
     val bubbleSort = obj.bubbleSort(list)
     assert(List(2, 4, 5, 6, 7) != bubbleSort)
   }
-
-  //Selection Sort
-  //Check with true give sorted Array
-  "it" should "match with value for selection sort" in {
-    val selectionSort = obj.selectionSort(list)
-    assert(List(2, 3, 5, 6, 7) == selectionSort)
-  }
-  //check with wrong output
-  "it" should "not match with value for selection sort" in {
-    val selectionSort = obj.selectionSort(list)
-    assert(List(2, 3, 2, 9, 7) != selectionSort)
-  }
-
-
 }
