5c6d16e8f4c4126986c58c989c6911b40a9dd997
Manish Mishra <124979629+ManishKnolder@users.noreply.github.com>
2023-03-17 23:41:33 +0530

    Update SortingProgram.scala

--- session_1/session-1-assignment-3/src/main/scala/SortingProgram.scala
+++ session_1/session-1-assignment-3/src/main/scala/SortingProgram.scala
@@ -29,5 +29,19 @@ object SortingProgram extends App {
         min :: selectionSort(list.filter(_ != min))
     }
   }
+    //This method is used for insertion sort
+  def insertionSort(list: List[Int]): List[Int] = {
+    list match {
+      case Nil => Nil
+      case head :: tail => insert(head, insertionSort(tail))
+    }
+  }
+
+  def insert(x: Int, arr: List[Int]): List[Int] = {
+    arr match {
+      case Nil => Nil
+      case head :: tail => if (x <= head) x :: arr else head :: insert(x, tail)
+    }
+  }
 }
 
